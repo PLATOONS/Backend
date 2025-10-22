e7a36c5d1f5fa54ef1244d00b1cbf12e567c98cc
JPOR14 <juanpabloorozco71@gmail.com>
2025-10-22 01:56:49 -0600

    Refactor fetchOrder to return OrderResponseDto

--- src/main/java/com/platoons/e_commerce/service/impl/OrderServiceImpl.java
+++ src/main/java/com/platoons/e_commerce/service/impl/OrderServiceImpl.java
@@ -1,8 +1,9 @@
 package com.platoons.e_commerce.service.impl;
+
 import java.util.List;
 import java.util.stream.Collectors;
+
 import com.platoons.e_commerce.dto.CreateOrderRequestDto;
-import com.platoons.e_commerce.dto.OrderDto;
 import com.platoons.e_commerce.dto.OrderResponseDto;
 import com.platoons.e_commerce.dto.UpdateOrderDto;
 import com.platoons.e_commerce.entity.Order;
@@ -29,10 +30,10 @@ public class OrderServiceImpl implements IOrderService {
     }
 
     @Override
-    public OrderDto fetchOrder(String orderId) {
+    public OrderResponseDto fetchOrder(String orderId) {
         var savedOrder = orderRepository.findById(Long.parseLong(orderId))
                 .orElseThrow(() -> new EntityNotFoundException("order", "orderId", orderId));
-        return OrderMapper.mapOrderToOrderDto(savedOrder, new OrderDto());
+        return OrderMapper.mapOrderToOrderResponseDto(savedOrder);
     }
 
     @Override
@@ -61,7 +62,7 @@ public class OrderServiceImpl implements IOrderService {
         var orders = orderRepository.findAllByCustomerUsernameAndDeletedAtIsNull(username);
 
         return orders.stream()
-                .map(order -> OrderMapper.mapOrderToOrderResponseDto(order, new OrderResponseDto()))
+                .map(OrderMapper::mapOrderToOrderResponseDto)
                 .collect(Collectors.toList());
     }
 }
